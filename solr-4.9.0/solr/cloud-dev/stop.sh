fb3888be50692d1b8da2cd358534fa14c9ad8bdc
williamchengit <cavenchen@gmail.com>
2014-06-30 10:07:42 +0800

    Add solr 4.9 as sub folder for test

---
+++ solr-4.9.0/solr/cloud-dev/stop.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+numServers=$1
+baseStopPort=6572
+
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+[ "$#" -eq 1 ] || die "1 argument required, $# provided, usage: stop.sh {numServers}"
+
+cd ../example
+
+java -DSTOP.PORT=7983 -DSTOP.KEY=key -jar start.jar --stop
+
+
+for (( i=2; i <= $numServers; i++ ))
+do
+  echo "starting example$i"
+  cd ../example$i
+  stopPort=`expr $baseStopPort + $i`
+  java -DSTOP.PORT=$stopPort -DSTOP.KEY=key -jar start.jar --stop
+done
